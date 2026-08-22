f5b6f302c13454c814a22a7dcbe6334b5f45f047
NeonVR-CTRL <tfr691245@gmail.com>
2026-08-23 00:03:38 +0530

    Delete ne7_sql directory

--- ne7_sql/src/boot.js
+++
@@ -1,6 +0,0 @@
-import { startAPI } from './backend/api.js';
-import { startTCP } from './backend/tcp.js';
-import { logger } from './core.js';
-logger.info('Booting NE7-SQL God-Tier Architecture...');
-startAPI(8080);
-startTCP(5432);
--- ne7_sql/src/diagnose.js
+++
@@ -1,8 +0,0 @@
-console.log('🔍 Diagnosing ESM initialization chain...\n');
-try {
-  const main = await import('./main.js');
-  console.log('✅ SUCCESS! main.js loaded. Exports:', Object.keys(main));
-} catch (e) {
-  console.error('❌ FATAL ERROR in main.js or its dependencies:');
-  console.error(e.stack || e);
-}
